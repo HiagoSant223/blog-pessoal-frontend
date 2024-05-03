a1c8afec153b0672a615d087abd85c5183523ca3
Hiago Oliveira <hiagosantos223@gmail.com>
2024-05-03 15:29:41 -0300

    Cookbook 18 a 23

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,33 @@ import Login from './pages/Login/Login';
 import Cadastro from './pages/Cadastro/Cadastro';
 import Home from './pages/Home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/Temas/ListaTemas/ListaTemas';
+import FormularioTema from './components/Temas/FormularioTemas/FormularioTema';
+import DeletarTema from './components/Temas/DeletarTemas/DeletarTema';
 
 
 function App() {
   return (
+    <>
     <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Cadastro />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-    </AuthProvider>
+        </AuthProvider>
+    </>
   );
 }
 export default App;
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,16 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
-// Conexão Front X Back
-const  api = axios.create({
-    baseURL: "https://blog-pessoal-64gy.onrender.com/"
-}); 
-
-export const login = async (url: string, dados: Object, setDados: Function) => {
-    const resposta = await api.post(url, dados);
-    setDados(resposta.data);
-}
+const api = axios.create({
+  baseURL: 'https://blog-pessoal-64gy.onrender.com/'
+})
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data)
-  }
+  const resposta = await api.post(url, dados)
+  setDados(resposta.data)
+}
+
+export const login = async(url: string, dados: Object, setDados: Function) => {
+  const resposta = await api.post(url, dados)
+  setDados(resposta.data)
+}
+
+export const buscar = async(url: string, setDados: Function, header: Object) => {
+  const resposta = await api.get(url, header)
+  setDados(resposta.data)
+}
+
+export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.post(url, dados, header)
+  setDados(resposta.data)
+}
+
+export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.put(url, dados, header)
+  setDados(resposta.data)
+}
+
+export const deletar = async(url: string, header: Object) => {
+  await api.delete(url, header)
+}
